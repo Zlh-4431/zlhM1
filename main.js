0b4a6ec60e669128483790b0ccac0b0e0552d033
Zlh-4431 <124013738+Zlh-4431@users.noreply.github.com>
2024-02-13 00:38:28 +0800

    1

--- main.js
+++ main.js
@@ -76,27 +76,4 @@ document.addEventListener("keydown", function (event) {
     moveContrl(event);
 })
 
-butt1.addEventListener("click", function () {
-    if (hi < 600) {
-        upB();
-    }
-    reC();
-})
-butt2.addEventListener("click", function () {
-    if (hi > 200) {
-        downB();
-    }
-    reC();
-})
-butt3.addEventListener("click", function () {
-    if (li > 450) {
-        leftB();
-    }
-    reC();
-})
-butt4.addEventListener("click", function () {
-    if (li < 1200) {
-        rightB();
-    }
-    reC();
-})
+
